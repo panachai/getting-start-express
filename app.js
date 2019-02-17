4bab0dfe2fad785f5c737e80f4da991107a93cae
Panachai <panachai.ny@gmail.com>
2019-02-17 16:41:39 +0700

    Body Parser

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
+
+const foodFullnesses = {
+  spaghetti: 2,
+  pizza: 2,
+  water: 0,
+  galicbread: 1,
+  caesarsalad: 3
+};
+
+let fullnessScore = 0;
+const fullness = function() {
+  return `
+    <div>
+        <label>Fullness: </label>${fullnessScore}
+    </div>
+`;
+};
+
+const form = `
+    <form action="/feed" method="POST">
+        <label for="food">Food: </label>
+        <input id="food" name="food"/>
+        <button type="submit">Feed</button>
+    </form>
+`;
 
 app.get("/", function(request, response) {
   response.send("Hello, guy");
 });
 
 app.get("/choco", function(request, response) {
-  response.send("<h1>Chocolate</h1>");
+  response.send(`<h1>Chocolate</h1> ${fullness()} ${form}`);
+});
+
+app.post("/feed", urlencodedParser, function(request, response) {
+  const food = request.body.food;
+
+  let foodScore = foodFullnesses[food.toLowerCase()] || 0;
+  fullnessScore += foodScore;
+  response.redirect("/choco");
 });
 
 app.get("/error", function(request, response) {
@@ -14,8 +49,8 @@ app.get("/error", function(request, response) {
 });
 
 app.all("*", function(request, response) {
-  //   response.redirect("/error");
-  response.send("asdasd" + request.method);
+  response.redirect("/error");
+  // response.send("asdasd" + request.method);
 });
 
 app.listen(3000);
